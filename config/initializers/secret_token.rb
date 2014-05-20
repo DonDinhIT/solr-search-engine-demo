84987e8ff65aec8544fe31686c86887165f73efa
Don Dinh <don.dinhnu@asnet.com.vn>
2014-05-20 17:26:35 +0700

    Create sunspot demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolrSearchDemon::Application.config.secret_key_base = '24f045c4627602f09bb24ce4ba67ea6143aae401e9cb2d85685c7d3730e5508dc2102037c93fa9be579828e9f0d59990702790f835d7d4c6211ea84078e1058b'
